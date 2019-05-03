d009031704572cb797286ca6b7b98c24682e5b09
lincal2309 <christophe.gressin@tofographies.com>
2019-05-03 11:04:10 +0200

    Preparation for deployment

--- goodfat/goodfat/settings.py
+++ goodfat/goodfat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,14 +20,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%amnse12$z#ph#5hwt3y6jo%la78wjk@g!uj6_afpgry+8ukwb'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG == True:
+    SECRET_KEY = '%amnse12$z#ph#5hwt3y6jo%la78wjk@g!uj6_afpgry+8ukwb'
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -129,3 +133,6 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = '/login/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
